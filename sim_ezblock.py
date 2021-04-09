35fdd13ed7e0795ca5a7ff0c1347bb8fa0509f0b
masafumiendo <endoma@oregonstate.edu>
2021-04-09 10:54:26 -0700

    added classes in sim_ezblock.py

--- sim_ezblock.py
+++ sim_ezblock.py
@@ -4,3 +4,214 @@ Date: 04/07/2021
 Description: a script to solve issues
 """
 
+
+class Servo():
+    def __init__(self, pwm):
+        pass
+
+    def angle(self, angle):
+        pass
+
+
+class PWM():
+
+    def __init__(self, channel, debug="critical"):
+        super().__init__()
+        if isinstance(channel, str):
+            if channel.startswith("P"):
+                channel = int(channel[1:])
+            else:
+                raise ValueError("PWM channel should be between [P1, P14], not {0}".format(channel))
+        try:
+            pass
+        except IOError:
+            pass
+
+    def i2c_write(self, reg, value):
+        pass
+
+    def freq(self, *freq):
+        if len(freq) == 0:
+            return self._freq
+        else:
+            pass
+
+    def prescaler(self, *prescaler):
+        if len(prescaler) == 0:
+            return self._prescaler
+        else:
+            pass
+
+    def period(self, *arr):
+        global timer
+        if len(arr) == 0:
+            return timer[self.timer]["arr"]
+        else:
+            pass
+
+    def pulse_width(self, *pulse_width):
+        if len(pulse_width) == 0:
+            return self._pulse_width
+        else:
+            pass
+
+    def pulse_width_percent(self, *pulse_width_percent):
+        global timer
+        if len(pulse_width_percent) == 0:
+            return self._pulse_width_percent
+        else:
+            pass
+
+
+class Pin():
+
+    PULL_NONE = None
+
+    _dict = {
+        "BOARD_TYPE": 12,
+    }
+
+    _dict_1 = {
+        "D0":  17,
+        "D1":  18,
+        "D2":  27,
+        "D3":  22,
+        "D4":  23,
+        "D5":  24,
+        "D6":  25,
+        "D7":  4,
+        "D8":  5,
+        "D9":  6,
+        "D10": 12,
+        "D11": 13,
+        "D12": 19,
+        "D13": 16,
+        "D14": 26,
+        "D15": 20,
+        "D16": 21,
+        "SW":  19,
+        "LED": 26,
+        "BOARD_TYPE": 12,
+        "RST": 16,
+        "BLEINT": 13,
+        "BLERST": 20,
+        "MCURST": 21,
+    }
+
+    _dict_2 = {
+        "D0":  17,
+        "D1":   4, # Changed
+        "D2":  27,
+        "D3":  22,
+        "D4":  23,
+        "D5":  24,
+        "D6":  25, # Removed
+        "D7":   4, # Removed
+        "D8":   5, # Removed
+        "D9":   6,
+        "D10": 12,
+        "D11": 13,
+        "D12": 19,
+        "D13": 16,
+        "D14": 26,
+        "D15": 20,
+        "D16": 21,
+        "SW":  25, # Changed
+        "LED": 26,
+        "BOARD_TYPE": 12,
+        "RST": 16,
+        "BLEINT": 13,
+        "BLERST": 20,
+        "MCURST":  5, # Changed
+    }
+
+    def __init__(self, *value):
+        super().__init__()
+        pass
+
+    def check_board_type(self):
+        pass
+
+    def init(self, mode, pull=PULL_NONE):
+        self._pull = pull
+        self._mode = mode
+
+    def dict(self, *_dict):
+        pass
+
+    def __call__(self, value):
+        return self.value(value)
+
+    def value(self, *value):
+        if len(value) == 0:
+            return 1
+        else:
+            return value
+
+    def on(self):
+        return self.value(1)
+
+    def off(self):
+        return self.value(0)
+
+    def high(self):
+        return self.on()
+
+    def low(self):
+        return self.off()
+
+    def mode(self, *value):
+        if len(value) == 0:
+            return (self._mode, self._pull)
+        else:
+            self._mode = value[0]
+            if len(value) == 1:
+                pass
+            elif len(value) == 2:
+                pass
+
+    def pull(self, *value):
+        return self._pull
+
+    def irq(self, handler=None, trigger=None, bouncetime=200):
+        pass
+
+    def name(self):
+        return 0
+
+    def names(self):
+        return [0, 0]
+
+    class cpu(object):
+        GPIO17 = 17
+        GPIO18 = 18
+        GPIO27 = 27
+        GPIO22 = 22
+        GPIO23 = 23
+        GPIO24 = 24
+        GPIO25 = 25
+        GPIO26 = 26
+        GPIO4  = 4
+        GPIO5  = 5
+        GPIO6  = 6
+        GPIO12 = 12
+        GPIO13 = 13
+        GPIO19 = 19
+        GPIO16 = 16
+        GPIO26 = 26
+        GPIO20 = 20
+        GPIO21 = 21
+
+        def __init__(self):
+            pass
+
+class ADC():
+
+    def __init__(self, chn):    # 参数，通道数，树莓派扩展板上有8个adc通道分别为"A0, A1, A2, A3, A4, A5, A6, A7"
+        super().__init__()
+
+    def read(self):                     # adc通道读取数---写一次数据，读取两次数据 （读取的数据范围是0~4095）
+        return 0
+
+    def read_voltage(self):                             # 将读取的数据转化为电压值（0~3.3V）
+        return 0
