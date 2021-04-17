393d3c675b779e0c1945e4ff80ed0526e0add3eb
masafumiendo <endoma@oregonstate.edu>
2021-04-16 23:18:33 -0700

    write intergator

--- integration.py
+++ integration.py
@@ -4,6 +4,10 @@ Date: 04/15/2021
 Content: class for integration (3.4)
 """
 
+from sensing import Sensor
+from interpretation import Interpretor
+from controller import Controller
+
 class Integrator:
 
     # initialization
@@ -14,11 +18,19 @@ class Integrator:
         self.speed = speed
         self.type_c = type_c
 
-    def integrator(self):
+    def feedback_controller(self):
         steer_angle = self.controller.controller(self.type_c)
+        print(steer_angle)
         self.sensor.car.forward(self.speed, steer_angle)
 
     def line_trace(self):
         # trace line while controlling steering angle
         while True:
-            self.integrator()
+            self.feedback_controller()
+
+if __name__ == '__main__':
+    sensor = Sensor()
+    interpretor = Interpretor()
+    controller = Controller(sensor, interpretor)
+    integrator = Integrator(sensor, controller, speed=20, type_c="pd")
+    integrator.line_trace()
--- interpretation.py
+++ interpretation.py
@@ -7,7 +7,7 @@ Content: class for interpretation (3.2)
 class Interpretor:
 
     # initialization
-    def __init__(self, sensitivity, polarity):
+    def __init__(self, sensitivity=0.8, polarity=0):
         """
         :param sensitivity: value from 0 to 1
         :param polarity: value of 0 (follow darker color) or 1 (follow lighter color)
@@ -16,4 +16,17 @@ class Interpretor:
         self.polarity = polarity
 
     def calc_relative_pos(self, vals):
-        return
+
+        # calculate relative differences against target (center) value
+        diff_l = vals[1] - vals[0]
+        diff_r = vals[1] - vals[2]
+
+        # follow darker color
+        pos = self.sensitivity * (diff_l - diff_r)
+        if self.polarity == 1:
+            # follow lighter color (change sign)
+            pos = - 1 * pos
+        if abs(pos) > 1:
+            pos = pos / abs(pos)
+
+        return pos
