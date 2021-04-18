ead8abf6784e928951f28571f1d875eaf7f6af88
masafumiendo <endoma@oregonstate.edu>
2021-04-18 01:59:09 +0100

    change main

--- main/main_sensing_and_control.py
+++ main/main_sensing_and_control.py
@@ -7,7 +7,7 @@ Content: main function to sense line and follow it by PID controller
 import sys
 sys.path.append('..')
 
-import time
+# import time
 
 from picarx_organized import PicarX
 from sensing import Sensor
@@ -24,4 +24,4 @@ if __name__ == '__main__':
         vals = sensor.sensor_reading()
         pos = interpretor.calc_relative_pos(vals)
         print('relative positioning error : {}'.format(pos))
-        time.sleep(0.1)
+        # time.sleep(0.1)
