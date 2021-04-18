e86eb4601fdab18207c5e1b7bedf4012a2380beb
masafumiendo <endoma@oregonstate.edu>
2021-04-17 19:03:01 -0700

    modify sensing class

--- sensing.py
+++ sensing.py
@@ -4,6 +4,17 @@ Date: 04/15/2021
 Content: class for sensing (3.1)
 """
 
+### begin 2.5.3
+try:
+    from ezblock import *
+    from ezblock import __reset_mcu__
+    __reset_mcu__()
+    time.sleep(0.01)
+except ImportError:
+    print("This computer does not appear to be a PiCar-X system(/opt/ezblock is not present). Shadowing hardware calls with substitute functions")
+    from sim_ezblock import *
+### end 2.5.3
+
 from picarx_organized import PicarX
 
 class Sensor:
@@ -11,7 +22,10 @@ class Sensor:
     # initialization
     def __init__(self, car):
         self.car = car
-        self.sensors = [self.car.S0, self.car.S1, self.car.S2]
+        self.S0 = ADC('A0')
+        self.S1 = ADC('A1')
+        self.S2 = ADC('A2')
+        self.sensors = [self.S0, self.S1, self.S2]
 
     def sensor_reading(self):
         return [sensor_.read() for sensor_ in self.sensors]
