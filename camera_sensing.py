3b169a303a120f3213ff9c14740fc06e545aced2
masafumiendo <endoma@oregonstate.edu>
2021-04-17 23:14:14 -0700

    added camera sensing class

--- camera_sensing.py
+++ camera_sensing.py
@@ -1,61 +1,41 @@
 """
 Authors: Masafumi Endo
 Date: 04/15/2021
-Content: classes for camera sensing (3.5)
+Content: classes for camera sensing and interpretation (3.5)
 """
 
-### begin 2.5.3
-try:
-    from ezblock import *
-    from ezblock import __reset_mcu__
-    __reset_mcu__()
-    time.sleep(0.01)
-except ImportError:
-    print("This computer does not appear to be a PiCar-X system(/opt/ezblock is not present). Shadowing hardware calls with substitute functions")
-    from sim_ezblock import *
-### end 2.5.3
+import numpy as np
+import cv2
 
-from picarx_organized import PicarX
+class CameraSensor:
 
-class Sensor:
+    # initialization
+    def __init__(self, camera):
+        self.camera = camera
+        self.resolution = (640, 480)
+
+    def sensor_reading(self, image):
+        return image
+
+class CameraInterpretor:
 
     # initialization
     def __init__(self):
-        self.S0 = ADC('A0')
-        self.S1 = ADC('A1')
-        self.S2 = ADC('A2')
-        self.sensors = [self.S0, self.S1, self.S2]
+        self.lower_blue = np.array([])
+        self.upper_blue = np.array([])
 
-    def sensor_reading(self):
-        return [sensor_.read() for sensor_ in self.sensors]
+    def calc_relative_pos(self, image):
 
-class Interpretor:
+        # create binary image from raw frame
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, self.lower_blue, self.upper_blue)
+        ret, th = cv2.threshold(mask, 0, 255, cv2.THRESH_OTSU)
 
-    # initialization
-    def __init__(self, sensitivity=1e-2, polarity=0):
-        """
-        :param sensitivity: value from 0 to 1
-        :param polarity: value of 0 (follow darker color) or 1 (follow lighter color)
-        """
-        self.sensitivity = sensitivity
-        self.polarity = polarity
+        # identify moment of line
+        mu = cv2.moments(th)
+        x, _ = int(mu["m10"]/mu["m00"]) , int(mu["m01"]/mu["m00"])
 
-    def calc_relative_pos(self, vals):
-
-        # set target value
-        if self.polarity == 0:
-            val_t = min(vals)
-        elif self.polarity == 1:
-            val_t = max(vals)
-
-        # calculate relative differences against target value
-        diff_l = vals[0] - val_t
-        diff_r = vals[2] - val_t
-        pos = self.sensitivity * (diff_l - diff_r)
-
-        if pos > 1:
-            pos = 1
-        elif pos < -1:
-            pos = -1
+        # based on the identified relative position, calculate relative position
+        pos = (320 - x) / 320
 
         return pos
