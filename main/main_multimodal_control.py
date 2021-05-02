044a2788b55447fe2c04844d418c5b7294a38b23
masafumiendo <endoma@oregonstate.edu>
2021-05-01 22:53:40 -0700

    Update main_multimodal_control.py

--- main/main_multimodal_control.py
+++ main/main_multimodal_control.py
@@ -36,10 +36,9 @@ if __name__ == '__main__':
 
     delay_time = 0.01
     with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
-        e1 = executor.submit(multimodal_executor.ultrasonic_sensing, ultra_producer, delay_time)
-        e2 = executor.submit(multimodal_executor.ultrasonic_interpretation, ultra_consumer, ultra_producer, delay_time)
-        e3 = executor.submit(multimodal_executor.collision_avoidance, ultra_consumer, delay_time)
-
-    e1.result()
-    e2.result()
-    e3.result()
+        executor.submit(multimodal_executor.photo_sensing, photo_producer, delay_time)
+        executor.submit(multimodal_executor.photo_interpretation, photo_consumer, photo_producer, delay_time)
+        executor.submit(multimodal_executor.steer_control, photo_consumer, delay_time)
+        # e1 = executor.submit(multimodal_executor.ultrasonic_sensing, ultra_producer, delay_time)
+        # e2 = executor.submit(multimodal_executor.ultrasonic_interpretation, ultra_consumer, ultra_producer, delay_time)
+        # e3 = executor.submit(multimodal_executor.collision_avoidance, ultra_consumer, delay_time)
