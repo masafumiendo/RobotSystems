c1333cfe715ba8f6b7fc25ffe93fa7265cd9e8b6
masafumiendo <endoma@oregonstate.edu>
2021-05-01 22:16:42 -0700

    print out consumer's message

--- multimodal_executer.py
+++ multimodal_executer.py
@@ -29,10 +29,9 @@ class MultimodalExecuter(ConcurrentExecuter):
 
         while True:
             val = bus_producer.read()
-            print(val)
             pos = self.ultrasonicinterpretor.calc_relative_pos(val)
-            print(pos)
             bus_consumer.write(pos)
+            print(bus_consumer.message)
             time.sleep(delay_time)
 
     def collision_avoidance(self, bus_consumer, delay_time):
