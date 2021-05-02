18cfa4ff5776d1efacf0ff17f748421655f0312b
masafumiendo <endoma@oregonstate.edu>
2021-05-01 23:07:56 -0700

    Update multimodal_executer.py

--- multimodal_executer.py
+++ multimodal_executer.py
@@ -37,5 +37,8 @@ class MultimodalExecuter(ConcurrentExecuter):
 
         while True:
             pos = bus_consumer.read()
-            print(pos)
+            if pos < 5:
+                self.car.stop()
+            else:
+                self.car.forward(10, 0)
             time.sleep(delay_time)
